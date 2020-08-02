2e9caefc9a7aa03523019eb7aabcb703ebada5f1
Andrew Hall <andrewbhall96@gmail.com>
2020-08-02 15:11:03 -0400

    created component files Navbar, Jumbotron, Card.js

--- src/components/HelloBootstrap.js
+++ src/components/HelloBootstrap.js
@@ -32,16 +32,17 @@ function HelloBootsrap() {
                 <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
                 <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
             </div>
-            <div className="card" style="width: 18rem;">
+            <div className="card">
                 <div className="card-header">
                     Featured
-  </div>
+                </div>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item">Cras justo odio</li>
                     <li className="list-group-item">Dapibus ac facilisis in</li>
                     <li className="list-group-item">Vestibulum at eros</li>
                 </ul>
             </div>
+
         </>
     );
 }
