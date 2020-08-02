ee7b1ee64710359cf30ad81da00b0162d64c5299
Andrew Hall <andrewbhall96@gmail.com>
2020-08-02 14:42:57 -0400

    created initial react files

---
+++ src/components/HelloBootstrap.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function HelloBootsrap() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className="jumbotron">
+                <h1 className="display-4">Hello, world!</h1>
+                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr className="my-4" />
+                <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
+                <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </div>
+        </  >
+    );
+}
+
+export default HelloBootsrap;
