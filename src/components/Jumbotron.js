76cf51039d65b42d86db215087ce512c548a62c3
Andrew Hall <andrewbhall96@gmail.com>
2020-08-04 14:17:39 -0400

    added new components

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -5,7 +5,7 @@ function Jumbotron() {
     return (
         
             <div className="jumbotron">
-                <h1 className="display-4">Make Your Day</h1>
+                <h1 className="display-4">Search Creators</h1>
                 <h2 className="lead">Real People. Real Videos.</h2>
                 <hr className="my-4" />
                 <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
