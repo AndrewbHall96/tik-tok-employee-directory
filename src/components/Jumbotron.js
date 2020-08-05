02a87ff7ec4ddfdacf34e48fd9eb73368958abb3
Andrew Hall <andrewbhall96@gmail.com>
2020-08-04 21:07:43 -0400

    added input element and creating search.filter

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -8,10 +8,12 @@ function Jumbotron() {
                 <h1 className="display-4">Search Creators</h1>
                 <h2 className="lead">Real People. Real Videos.</h2>
                 <hr className="my-4" />
-                <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                <input type="text" />
+                <button className="btn btn-primary btn-lg" role="button" onclick="searchCreators()">Learn more</button>
             </div>
             
     );
 }
+
 
 export default Jumbotron;
